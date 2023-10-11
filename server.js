05692f1c32da7349122f38b1eeb4d548e52516ae
kenny <kennysekhon@kennys-Mac-Pro-4.local>
2023-10-11 16:08:50 -0700

    Books Create/Index/New

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -27,6 +28,7 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(process.env.port || 3000);
 // saw this suggestion in https://stackoverflow.com/questions/14322989/first-heroku-deploy-failed-error-code-h10
